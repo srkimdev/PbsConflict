13e99fbe4dbe0377a308ff8395d0983edf8876b2
srkimdev <emirated3858@gmail.com>
2024-10-08 11:28:20 +0900

    init

---
+++ PbxConflict/PbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictApp.swift
+//  PbxConflict
+//
+//  Created by 김성률 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
